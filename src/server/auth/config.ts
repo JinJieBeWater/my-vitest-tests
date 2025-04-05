92c06cc52b18960d4504c770be33eee4fbedcc2d
JinJieBeWater <cjj20030813@qq.com>
2025-04-05 20:31:48 +0800

    ✨ feat(auth): 使用 GitHub Provider 替换 Discord Provider
    - 【更新】使用 GitHub 进行身份验证，替换之前的 Discord Provider
    - 【修改】.env 示例文件以反映 GitHub 凭据
    - 【更新】环境变量配置以使用 GitHub ID 和密钥

--- src/server/auth/config.ts
+++ src/server/auth/config.ts
@@ -1,7 +1,6 @@
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import type { DefaultSession, NextAuthConfig } from "next-auth";
-import DiscordProvider from "next-auth/providers/discord";
-
+import GitHubProvider from "next-auth/providers/github";
 import { db } from "@/server/db";
 import {
 	accounts,
@@ -38,7 +37,10 @@ declare module "next-auth" {
  */
 export const authConfig = {
 	providers: [
-		DiscordProvider,
+		GitHubProvider({
+			clientId: process.env.AUTH_GITHUB_ID,
+			clientSecret: process.env.AUTH_GITHUB_SECRET,
+		}),
 		/**
 		 * ...add more providers here.
 		 *
