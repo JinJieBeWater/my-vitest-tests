6721f4077d9027217244dec910efe0e855580e89
JinJieBeWater <cjj20030813@qq.com>
2025-04-07 02:23:06 +0800

    ♻️ refactor(trpc): optional db
    - 【删除】移除了`db`的可选类型，确保数据库连接的强制存在

--- src/server/api/trpc.ts
+++ src/server/api/trpc.ts
@@ -38,17 +38,16 @@ export const createTRPCContext = async (opts: { headers: Headers }) => {
 };
 
 export function createContextInner(opts: {
-	session: Session | null;
-	db?: typeof db;
+  session: Session | null
+  db: typeof db
 }) {
-	const headers = new Headers();
-	return {
-		db: opts.db ?? db,
-		session: opts.session,
-		headers,
-	};
+  const headers = new Headers()
+  return {
+    db: opts.db,
+    session: opts.session,
+    headers,
+  }
 }
-
 /**
  * 2. INITIALIZATION
  *
